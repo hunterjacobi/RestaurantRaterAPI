22707d5e112f0d53aa739a1d87c179e10c7cdb14
Hunter Browning <guccigod@Hunters-MacBook-Pro.local>
2022-03-15 23:27:44 -0400

    API Finished

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,105 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantRaterAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class RestaurantController : Controller
+    {
+        private RestaurantDbContext _context;
+        public RestaurantController(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> PostRestaurant([FromForm] RestaurantEdit model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Restaurants.Add(new Restaurant()
+            {
+                Name = model.Name,
+                Location = model.Location,
+            });
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetRestaurants()
+        {
+            var restaurant = await _context.Restaurants.ToListAsync();
+            return Ok();
+        }
+
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetRestaurantById(int id)
+        {
+            var restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(restaurant);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateRestaurant([FromForm] RestaurantEdit model, [FromRoute] int id)
+        {
+            var oldRestaurant = await _context.Restaurants.FindAsync(id);
+
+            if (oldRestaurant == null)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if (!string.IsNullOrEmpty(model.Name))
+            {
+                oldRestaurant.Name = model.Name;
+            }
+
+            if (!string.IsNullOrEmpty(model.Location))
+            {
+                oldRestaurant.Location = model.Location;
+            }
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteRestaurant([FromRoute] int id)
+        {
+            var restaurant = await _context.Restaurants.FindAsync(id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            _context.Restaurants.Remove(restaurant);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
